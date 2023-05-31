589d68799bb55ac95d118badffc8af43bed19440
Manojkumar886 <maddymano434@gmail.com>
2023-05-31 11:42:31 +0530

    First Commit

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+    <ol>
+      Web development
+      <li>Html</li>
+      <li>Css</li>
+      <li>Bootstrap</li>
+      <li>Java Script</li>
+    </ol>
+    </>
+  );
+}
+
+export default App;
